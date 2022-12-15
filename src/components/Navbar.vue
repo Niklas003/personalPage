3349ea4030df3626a79a8b40da6cb372cf4ad427
Niklas <nlabitzke77@gmail.com>
2022-12-15 23:38:52 +0100

    WIP

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,8 +10,7 @@
     <div class="text-center text-lg mx-1.5">
         <span v-if="site != 'Home'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"><RouterLink to="/">Home </RouterLink> </span><span v-if="site != 'Home'" class="inter-light">|</span>
         <span v-if="site != 'About'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"><RouterLink to="/about"> About </RouterLink> </span><span v-if="site != 'About'" class="inter-light">|</span>
-        <span v-if="site != 'Techstack'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"> <RouterLink to="/techstack"> Techstack </RouterLink> </span><span v-if="site != 'Techstack'" class="inter-light">|</span>
-        <span v-if="site != 'Platforms'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"> <RouterLink to="/platforms"> Platforms </RouterLink></span><span v-if="site != 'Platforms'" class="inter-light">|</span>
-        <span v-if="site != 'Contact'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"> Contact </span>
+        <span v-if="site != 'Techstack'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"> <RouterLink to="/techstack"> Techstack </RouterLink> </span><span v-if="site != 'Techstack' && site != 'Platforms'" class="inter-light">|</span>
+        <span v-if="site != 'Platforms'" class="inter-light hover:text-slate-400 transition duration-200 cursor-pointer"> <RouterLink to="/platforms"> Platforms </RouterLink></span>
       </div>
 </template>
