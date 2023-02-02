410ea280d6d2f61e33b5a896e7a08c0e060cc656
Niklas <nlabitzke77@gmail.com>
2023-02-02 09:24:12 +0100

    resolved Image problem

--- src/views/PlatformsView.vue
+++ src/views/PlatformsView.vue
@@ -11,9 +11,9 @@ import NavbarVue from '@/components/Navbar.vue';
 
         <div class="flex flex-col justify-center">
             <div class="flex-initial inline-block mx-auto w-max pt-32">
-            <a class="grayscale hover:grayscale-0 duration-300 px-4" href="https://github.com/Niklas003"><img class="github-shadow duration-300 inline" src="src\assets\github-mark-white.png" style="width: 3rem" alt="GitHub Logo"></a>
-            <a class="grayscale hover:grayscale-0 duration-300 px-4" href="https://gitlab.com/Niklas003"><img class="gitlab-shadow duration-300 inline" src="src\assets\gitlab-logo-500.svg" style="width: 4.3rem" alt="GitLab Logo"></a>
-            <a class="grayscale hover:grayscale-0 duration-300 px-4" href="https://www.linkedin.com/in/niklas-labitzke-a80b89256/"><img class="linkedIn-shadow duration-300 inline" src="src\assets\LinkedIn_logo_initials.png" style="width: 3rem" alt="LinkedIn Logo"></a>
+            <a class="grayscale hover:grayscale-0 duration-300 px-4" href="https://github.com/Niklas003"><img class="github-shadow duration-300 inline" src="../assets/Images/github-mark-white.png" style="width: 3rem" alt="GitHub Logo"></a>
+            <a class="grayscale hover:grayscale-0 duration-300 px-4" href="https://gitlab.com/Niklas003"><img class="gitlab-shadow duration-300 inline" src="../assets/Images/gitlab-logo-500.svg" style="width: 4.3rem" alt="GitLab Logo"></a>
+            <a class="grayscale hover:grayscale-0 duration-300 px-4" href="https://www.linkedin.com/in/niklas-labitzke-a80b89256/"><img class="linkedIn-shadow duration-300 inline" src="../assets/Images/LinkedIn_logo_initials.png" style="width: 3rem" alt="LinkedIn Logo"></a>
         </div>
         </div>
 
